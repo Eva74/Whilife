14d34cf38c469fe5253b684007196be8e21ced45
Eva <emsalas14@gmail.com>
2019-08-01 16:05:35 -0500

    Add dependentFile.java and willDelete.java

---
+++ willDelete.java
@@ -0,0 +1,9 @@
+public class willDelete extends sumAllEclipse{
+	public static int dependent() {
+		int f = 0;
+		int g = 0;
+		int x = sumAllThese(f, g);
+		System.out.println("This is dependent on willDelete");
+		return x;
+	}
+}
